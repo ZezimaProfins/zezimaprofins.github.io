cd2d3541e759b701efdc802090c602214569f7ab
ZezimaProfins <edgarskiecis@gmail.com>
2025-09-29 17:42:23 +0300

    Update discord.js

--- discord.js
+++ discord.js
@@ -5,7 +5,7 @@ window.addEventListener('load', () => {
         let id = widget.getAttribute('id') ?? null;
         let width = widget.getAttribute('width') ?? '400px';
         let height = widget.getAttribute('height') ?? '500px';
-        let footerText = widget.getAttribute('footerText') ?? 'WWG.LV';
+        let footerText = widget.getAttribute('footerText') ?? 'MU.WWG.LV';
         let color = widget.getAttribute('color') ?? '#5865f2';
         let backgroundColor = widget.getAttribute('backgroundColor') ?? '#0c0c0d';
         let textColor = widget.getAttribute('textColor') ?? '#fff';
@@ -81,4 +81,5 @@ function LDColor(color, percent) {
     return (0x1000000 + (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 + (B < 255 ? (B < 1 ? 0 : B) : 255) * 0x100 + (G < 255 ? (G < 1 ? 0 : G) : 255)).toString(16).slice(1);
 
 }
+
 
